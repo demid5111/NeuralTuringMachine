ee2bac6544bebd4f8c11440770e111e9e09abae6
Alexander Demidovskij <monadv@yandex.ru>
2021-01-19 12:25:03 +0300

    minor improvements

--- evaluate.py
+++ evaluate.py
@@ -2,17 +2,19 @@ import pickle
 
 import constants
 
-def run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator, args, target_point, labels, outputs, inputs, batches, store_heat_maps=False, generalization_num=None):
+
+def run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator, args,
+             target_point, labels, outputs, inputs, batches, store_heat_maps=False, generalization_num=None):
     task_loss = 0
     task_error = 0
     num_batches = len(batches)
     for seq_len, inputs, labels in batches:
         task_loss_, outputs = sess.run([model.loss, model.outputs],
-            feed_dict={
-                inputs_placeholder: inputs,
-                outputs_placeholder: labels,
-                max_seq_len_placeholder: seq_len
-            })
+                                       feed_dict={
+                                           inputs_placeholder: inputs,
+                                           outputs_placeholder: labels,
+                                           max_seq_len_placeholder: seq_len
+                                       })
 
         task_loss += task_loss_
         task_error += data_generator.error_per_seq(labels, outputs, args.batch_size)
@@ -37,16 +39,16 @@ def run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_p
             })
             pickle.dump(tmp, open(constants.GENERALIZATION_HEAD_LOG_FILE, "wb"))
 
-
     task_loss /= float(num_batches)
     task_error /= float(num_batches)
     return task_loss, task_error
 
 
-def eval_performance(sess, data_generator, args, model, target_point, labels, outputs, inputs, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, curriculum_point, store_heat_maps=False):
+def eval_performance(sess, data_generator, args, model, target_point, labels, outputs, inputs, inputs_placeholder,
+                     outputs_placeholder, max_seq_len_placeholder, curriculum_point, store_heat_maps=False):
     # target task
     batches = data_generator.generate_batches(
-        int(int(args.eval_batch_size/2)/args.batch_size),
+        int(int(args.eval_batch_size / 2) / args.batch_size),
         args.batch_size,
         bits_per_vector=args.num_bits_per_vector,
         curriculum_point=None,
@@ -71,7 +73,7 @@ def eval_performance(sess, data_generator, args, model, target_point, labels, ou
     # multi-task
 
     batches = data_generator.generate_batches(
-        int(args.eval_batch_size/args.batch_size),
+        int(args.eval_batch_size / args.batch_size),
         args.batch_size,
         bits_per_vector=args.num_bits_per_vector,
         curriculum_point=None,
@@ -80,12 +82,14 @@ def eval_performance(sess, data_generator, args, model, target_point, labels, ou
         pad_to_max_seq_len=args.pad_to_max_seq_len
     )
 
-    multi_task_loss, multi_task_error = run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator, args, target_point, labels, outputs, inputs, batches)
+    multi_task_loss, multi_task_error = run_eval(sess, model, inputs_placeholder, outputs_placeholder,
+                                                 max_seq_len_placeholder, data_generator, args, target_point, labels,
+                                                 outputs, inputs, batches)
 
     # curriculum point
     if curriculum_point is not None:
         batches = data_generator.generate_batches(
-            int(int(args.eval_batch_size/4)/args.batch_size),
+            int(int(args.eval_batch_size / 4) / args.batch_size),
             args.batch_size,
             bits_per_vector=args.num_bits_per_vector,
             curriculum_point=curriculum_point,
@@ -94,14 +98,17 @@ def eval_performance(sess, data_generator, args, model, target_point, labels, ou
             pad_to_max_seq_len=args.pad_to_max_seq_len
         )
 
-        curriculum_point_loss, curriculum_point_error = run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator, args, target_point, labels, outputs, inputs, batches)
+        curriculum_point_loss, curriculum_point_error = run_eval(sess, model, inputs_placeholder, outputs_placeholder,
+                                                                 max_seq_len_placeholder, data_generator, args,
+                                                                 target_point, labels, outputs, inputs, batches)
     else:
         curriculum_point_error = curriculum_point_loss = None
 
     return target_task_error, target_task_loss, multi_task_error, multi_task_loss, curriculum_point_error, curriculum_point_loss
 
 
-def eval_generalization(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator, args, target_point, labels, outputs, inputs):
+def eval_generalization(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator,
+                        args, target_point, labels, outputs, inputs):
     res = []
     seq_lens = []
     if args.task == 'copy':
@@ -120,6 +127,8 @@ def eval_generalization(sess, model, inputs_placeholder, outputs_placeholder, ma
             pad_to_max_seq_len=False
         )
 
-        loss, error = run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder, data_generator, args, target_point, labels, outputs, inputs, batches, store_heat_maps=args.verbose, generalization_num=i)
+        loss, error = run_eval(sess, model, inputs_placeholder, outputs_placeholder, max_seq_len_placeholder,
+                               data_generator, args, target_point, labels, outputs, inputs, batches,
+                               store_heat_maps=args.verbose, generalization_num=i)
         res.append(error)
     return res
