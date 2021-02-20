c09b56e10c9d924e3dea54da43d6460483b4e6f4
Alexander Demidovskij <monadv@yandex.ru>
2021-02-20 16:03:04 +0300

    added script for vps

---
+++ vps.sh
@@ -0,0 +1,6 @@
+# nohup bash vps.sh > out.log &
+
+python run_tasks.py --experiment_name experiment --verbose no \
+                      --num_train_steps 100000 --steps_per_eval 1000 --use_local_impl yes \
+                      --curriculum none \
+                      --num_bits_per_vector 3 --max_seq_len 10 --task sum
