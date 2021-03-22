26079921c9ddba93aaaa94ff640fb1f92e48b1eb
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-03-22 18:30:23 +0300

    added new parameter

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,6 @@ mkdir models
 python run_tasks.py --experiment_name experiment --verbose no \
                       --num_train_steps 100000 --steps_per_eval 1000 --use_local_impl yes \
                       --curriculum none \
-                      --num_bits_per_vector 3 \
+                      --num_bits_per_vector 3 --num_memory_locations 128 \
                       --max_seq_len 4 --task average_sum \
                       --num_experts 3
