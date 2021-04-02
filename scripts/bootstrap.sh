4f76dba8929f4011df95b44d1fb7d40548e8e3fa
Alexander Demidovskij <monadv@yandex.ru>
2021-04-02 12:49:20 +0300

    agree on installation 3

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,11 +7,11 @@ set -x
 
 # Preparing fresh machine for the first usage:
 
-sudo apt-get --yes --force-yes update
-sudo apt-get --yes --force-yes upgrade
-sudo add-apt-repository --yes --force-yes ppa:deadsnakes/ppa
-sudo apt-get --yes --force-yes install python3.7
-sudo apt-get --yes --force-yes install python3-pip
+sudo apt-get --yes update
+sudo apt-get --yes upgrade
+sudo add-apt-repository --yes ppa:deadsnakes/ppa
+sudo apt-get --yes install python3.7
+sudo apt-get --yes install python3-pip
 python3.7 -m pip install virtualenv
 python3.7 -m virtualenv venv
 source venv/bin/activate
