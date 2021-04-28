c40177344bb7caf6f769a315498698f3c8877c1a
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-04-28 09:57:23 +0300

    transition to TF2 in requirements, collect GPU information

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,6 +10,9 @@ free
 # get CPU information
 lscpu
 
+# get GPU information
+lspci -nn | grep '\[03'
+
 # run the training
 python3 run_tasks.py --experiment_name experiment --verbose no \
                       --num_train_steps 1000000 --steps_per_eval 10000 --use_local_impl yes \
