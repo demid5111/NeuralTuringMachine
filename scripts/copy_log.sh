e54903f25f9a8c7c711a0a823f6b7594426c481a
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-07-16 19:24:39 +0300

    updated submodule before training

--- scripts/copy_log.sh
+++ scripts/copy_log.sh
@@ -4,4 +4,4 @@ set -x
 WHERE_TO_KEEP=~/Downloads/new_log/
 rm -rf ${WHERE_TO_KEEP}
 mkdir -p ${WHERE_TO_KEEP}
-scp -r root@161.97.147.172:~/NeuralTuringMachine/out.log ${WHERE_TO_KEEP}
+scp -r root@188.124.51.160:~/NeuralTuringMachine/out.log ${WHERE_TO_KEEP}
